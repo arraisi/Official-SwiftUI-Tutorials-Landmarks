a5b5ed08e2916c2ca77542fc1274804ea425f59a
Abdul R. Arraisi <itsarraisi@gmail.com>
2020-12-17 15:21:24 +0700

    Building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Abdul R. Arraisi on 17/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
