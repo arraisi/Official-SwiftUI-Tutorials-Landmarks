e50b417bc6ca6b67c029c9be24c47c4bb82e8203
Abdul R. Arraisi <itsarraisi@gmail.com>
2020-12-17 13:46:03 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Abdul R. Arraisi on 17/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
